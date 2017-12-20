c5dc504313da2f118c01f9b6a5bf62e27e971cee
Mark Cruse <mark.cruse@uky.edu>
2017-12-20 07:19:32 -0500

    Remove point data with 0 Probability of Snow and 0 Median Snow

--- parse-process-csv-point-data.js
+++ parse-process-csv-point-data.js
@@ -1,8 +1,9 @@
 var csv2geojson = require('csv2geojson'),
-    fs = require('fs');
+    fs = require('fs'),
+    path = require('path');
 
 // read file as string
-fs.readFile('project-files/Christmas_Snow_Statistics_2016.csv', 'utf-8', (err, csvString) => {
+fs.readFile(path.join(__dirname, '/project-files/') + 'Christmas_Snow_Statistics_2016.csv', 'utf-8', (err, csvString) => {
 
     if (err) throw err;
 
@@ -17,11 +18,11 @@ fs.readFile('project-files/Christmas_Snow_Statistics_2016.csv', 'utf-8', (err, c
 
         if (err) throw err;
 
-        var appendGeoJSON = filterFields(geojson);
+        var appendGeoJSON = formatFields(geojson);
         // console.log(appendGeoJSON); // this is our geojson!
 
         // write file
-        fs.writeFile('data/xmas_snow_stats_point_data.json', JSON.stringify(appendGeoJSON), 'utf-8', (err) => {
+        fs.writeFile(path.join(__dirname, '/data/') + 'xmas_snow_stats_point_data.json', JSON.stringify(appendGeoJSON), 'utf-8', (err) => {
 
             if (err) throw err;
 
@@ -30,11 +31,10 @@ fs.readFile('project-files/Christmas_Snow_Statistics_2016.csv', 'utf-8', (err, c
     })
 });
 
-function filterFields(geojson) {
+function formatFields(geojson) {
     // shorthand to our features
     var features = geojson.features,
         newFeatures = []; // empty array for new features
-
     // loop through all the features
     features.forEach((feature) => {
         // on each loop, create an empty object
@@ -42,33 +42,58 @@ function filterFields(geojson) {
         // loop through each of the properties for features names that contain text with %
         // convert each field to decimal number
         for (var prop in feature.properties) {
-            // if it's a match
-            if (prop === 'Probability_of_Snow_Depth_GT_0' || prop === 'Probability_of_Snowfall_GT_0') {
+            // change elevation from text field to integer
+            if (prop === 'Elev')
+                tempProps[prop] = parseFloat(feature.properties[prop]);
+            // if it's a match on either probability field
+            else if (prop === 'Probability_of_Snow_Depth_GT_0' || prop === 'Probability_of_Snowfall_GT_0') {
 
                 if (feature.properties[prop] === '-9999' || feature.properties[prop] === '-6666' || feature.properties[prop] === '-5555')
                     tempProps[prop] = 0;
                 else
-                tempProps[prop] = (parseInt(feature.properties[prop]))/100;
-
+                    tempProps[prop] = (parseInt(feature.properties[prop])) / 100;
+                // if it's a match on either median field
             } else if (prop === 'Median_of_Snow_Depth_GT_0_inches' || prop === 'Median_of_Snowfall_GT_0_inches') {
                 if (feature.properties[prop] === '-9999' || feature.properties[prop] === '-6666' || feature.properties[prop] === '-5555')
                     tempProps[prop] = 0;
-                else 
-                tempProps[prop] = feature.properties[prop];
+                else
+                    tempProps[prop] = parseInt(feature.properties[prop]);
+                // populate the array with other property fields
             } else
                 // pass the other prop values
                 tempProps[prop] = feature.properties[prop];
-            console.log(tempProps)
         }
+        var zone = 0;
+        // Calculate the zone based on probablility of snow
+        if (tempProps.Probability_of_Snow_Depth_GT_0 > .89)
+            zone = 7;
+        else if (tempProps.Probability_of_Snow_Depth_GT_0 > .74)
+            zone = 6;
+        else if (tempProps.Probability_of_Snow_Depth_GT_0 > .49)
+            zone = 5;
+        else if (tempProps.Probability_of_Snow_Depth_GT_0 > .29)
+            zone = 4;
+        else if (tempProps.Probability_of_Snow_Depth_GT_0 > .19)
+            zone = 3;
+        else if (tempProps.Probability_of_Snow_Depth_GT_0 > .04)
+            zone = 2;
+        else if (tempProps.Probability_of_Snow_Depth_GT_0 > 0)
+            zone = 1;
+        // Add the property zone
+        tempProps.Zone = zone;
         // now push a new feature to the newFeatures array
         // we will use the existing feature type and geometry,
         // but we can use our new properties as the "properties" value
-        newFeatures.push({
-            "type": feature.type,
-            "geometry": feature.geometry,
-            "properties": tempProps
-        });
-    });
+        // only include zones > 0
+        if (tempProps.Zone > 0 && tempProps.Median_of_Snow_Depth_GT_0_inches > 0) {
+            newFeatures.push({
+                "type": feature.type,
+                "geometry": feature.geometry,
+                "properties": tempProps
+            });
+            console.log(tempProps);
+        }
+    });  // features loop
     // finally, return a GeoJSON object FeatureCollection,
     // using the new features as the "features" value
     return {
